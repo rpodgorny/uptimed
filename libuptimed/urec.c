81e41bf0e73081dd14309e64017b7ae61353e325
Radek Podgorny <radek@podgorny.cz>
2007-11-22 14:39:37 +0100

    Added a list of files to ignore (thanks to Jeroen van Meeuwen)

---
+++ libuptimed/urec.c
@@ -0,0 +1,468 @@
+/*
+uptimed - Copyright (c) 1998-2004 Rob Kaper <rob@unixcode.org>
+        - read_uptime code for BSD and Solaris platforms taken from upclient
+          package by Martijn Broenland.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 dated June, 1991.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program;  if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave., Cambridge, MA 02139, USA.
+*/
+
+#include "../config.h"
+#include "urec.h"
+
+Urec *urec_list = NULL;
+static Urec *urec_last = NULL;
+
+Urec *add_urec(time_t utime, time_t btime, char *sys) {
+	Urec *u, *tmpu, *uprev = NULL;
+
+	/* Allocate memory for the new entry. */
+	if ((u=malloc(sizeof(Urec))) == NULL) {
+		printf("error mallocing urec struct. this is serious shit! exiting.\n");
+		exit(1);
+	}
+
+	/* Copy boottime, uptime and systeminfo into memory. */
+	u->utime = utime;
+	u->btime = btime;
+	strncpy(u->sys, sys, SYSMAX);
+	u->sys[SYSMAX]='\0';
+
+	/* Add the entry to the linked list. */
+	for(tmpu = urec_list; tmpu; tmpu = tmpu->next) {
+		if (u->utime > tmpu->utime) {
+			u->next=tmpu;
+			if (tmpu == urec_list) return urec_list = u;
+			return uprev->next = u;
+		} else {
+			uprev = tmpu;
+		}
+	}
+
+	u->next = NULL;
+	if (urec_last) {
+		urec_last->next = u;
+	} else {
+		urec_list = u;
+	}
+	return urec_last = u;
+}
+
+void del_urec(Urec *u) {
+	Urec *tmpu = urec_list;
+	
+	if (u == urec_list) {
+		urec_list=u->next;
+		if (!urec_list) urec_last = NULL;
+	} else {
+		for (tmpu = urec_list; tmpu->next && u != tmpu->next; tmpu = tmpu->next);
+		if (!u->next) urec_last = tmpu;
+		tmpu->next = u->next;
+	}
+	free(u);
+}
+
+void moveup(void) {
+	/* Delete current session from the list. */
+	del_urec(u_current);
+
+	/* Re-add it. (it should be urec_list now) */
+	u_current=add_urec(read_uptime(), readbootid(), read_sysinfo());
+}
+
+char *read_sysinfo(void) {
+	struct utsname temp_uname;
+	static char sys[SYSMAX+1];
+
+	/* What kernel are we running at the moment? */
+	if (!uname(&temp_uname)) {
+		/* Name and number.. */
+		snprintf(sys, SYSMAX, "%s %s", temp_uname.sysname, temp_uname.release);
+		sys[SYSMAX]='\0';
+		return sys;
+	} else {
+#ifdef PLATFORM_LINUX
+		return "Linux";
+#endif
+#ifdef PLATFORM_BSD
+		return "BSD";
+#endif
+#ifdef PLATFORM_SOLARIS
+		return "Solaris";
+#endif
+#ifdef PLATFORM_HPUX
+		return "HP/UX";
+#endif
+#ifdef PLATFORM_UNKNOWN
+		return "unknown";
+#endif
+	}
+}
+
+#ifdef PLATFORM_LINUX
+time_t read_uptime(void) {
+	struct sysinfo	si;
+
+	/* Until jiffies is declared to something different than unsigned long
+	 * in the kernel sources, this value will probably on all 32b platforms
+	 * wrap past approx. 497 days. This also applies to reading this value
+	 * through /proc/uptime.
+	 */
+	if (sysinfo(&si) != 0) {
+		printf ("uptimed: error getting uptime!\n");
+		exit(-1);
+	}
+	return((time_t)si.uptime);
+}
+#endif
+
+#ifdef PLATFORM_BSD
+time_t read_uptime(void) {
+	time_t now, up;
+	struct timeval boottime;
+	int mib[2];
+	size_t size;
+
+	(void)time(&now);
+	mib[0] = CTL_KERN;
+	mib[1] = KERN_BOOTTIME;
+	size = sizeof (boottime);
+	if (sysctl (mib, 2, &boottime, &size, NULL, 0) != -1 && boottime.tv_sec!= 0) {
+		up = now - boottime.tv_sec;
+	}
+	
+	return up;
+}
+#endif
+
+#ifdef PLATFORM_SOLARIS
+time_t read_uptime(void) {
+	int fd;
+	struct utmp ut;
+	int found=0;
+
+	fd = open (UTMP_FILE, O_RDONLY);
+	if (fd >= 0) {
+		while (!found) {
+			if (read(fd, &ut, sizeof(ut)) < 0) {
+				found = -1;
+			} else if (ut.ut_type==BOOT_TIME) {
+				found = 1;
+			}
+		}
+		close(fd);
+	}
+
+	if (found == 1) return time(0) - ut.ut_time;
+
+	return 0;
+}
+#endif
+
+#ifdef PLATFORM_HPUX
+time_t read_uptime(void) {
+	struct pst_static _pst_static;
+	pstat_getstatic( &_pst_static, sizeof(_pst_static), (size_t)1, 0);
+	return (time_t)(time(0) - _pst_static.boot_time);
+}
+#endif
+
+#ifdef PLATFORM_UNKNOWN
+time_t read_uptime(void) {
+/*
+ * This is a quick and inaccurate hack calculating the uptime from the
+ * current time and the timestamp made at boottime.
+ *
+ * This is inaccurate because:
+ * 1) the boottime timestamp can be extremely delayed due to fscking etc.
+ * 2) when the system time changes (for example timezone changes) the
+ *    boottime timestamp does not, creating even more inaccuracy.
+ */
+ 	if (u_current) return time(0) - u_current->btime;
+
+	return time(0) - readbootid();
+}
+#endif
+
+void read_records(time_t current) {
+	FILE *f;
+	char str[256];
+	time_t utime, btime;
+	long l_utime, l_btime;
+	char buf[256], sys[SYSMAX+1];
+	
+	f=fopen(FILE_RECORDS, "r");
+	if (!f) return;
+	
+	fgets(str, sizeof(str), f);
+	while (!feof(f)) {
+		/* Check for validity of input string. */
+		if (sscanf(str, "%ld:%ld:%[^]\n]", &l_utime, &l_btime, buf) != 3) {
+			/* Skip this entry. Do we want feedback here? */
+		} else {
+			utime = (time_t)l_utime;
+			btime = (time_t)l_btime;
+
+			strncpy(sys, buf, SYSMAX);
+			sys[SYSMAX]='\0';
+			if (utime > 0 && btime != current) add_urec(utime, btime, sys);
+		}
+		fgets(str, sizeof(str), f);
+	}
+	fclose(f);
+}
+
+void save_records(int max, time_t log_threshold) {
+	FILE *f;
+	Urec *u;
+	int i = 0;
+	
+	f = fopen(FILE_RECORDS".tmp", "w");
+	if (!f) {
+		printf("uptimed: cannot write to %s\n", FILE_RECORDS);
+		return;
+	}
+
+	for(u=urec_list; u; u = u->next) {
+		/* Ignore everything below the threshold */
+		if (u->utime >= log_threshold) {
+			fprintf(f, "%lu:%lu:%s\n", (unsigned long)u->utime, (unsigned long)u->btime, u->sys);
+			/* Stop processing when we've logged the max number specified. */
+			if ((max > 0) && (++i >= max)) break;
+		}
+	}
+	fclose(f);
+	rename(FILE_RECORDS".tmp", FILE_RECORDS);
+}
+
+#ifdef PLATFORM_LINUX
+int createbootid(void) {
+	FILE *f;
+	char str[256];
+	time_t bootid = 0;
+	
+	f=fopen("/proc/stat", "r");
+	if (!f) {
+		printf ("Error opening /proc file. Can not determine bootid, exiting!\n"); exit(-1);
+	} else {
+		fgets(str, sizeof(str), f);
+		while (!feof(f)) {
+			if (strstr(str, "btime")) {
+				bootid=atoi(str+6);
+				break;
+			}
+			fgets(str, sizeof(str), f);
+		}
+		fclose(f);
+	}
+	
+	f = fopen(FILE_BOOTID, "w");
+	if (!f) {
+		printf("Error writing bootid file, exiting!\n");  exit(-1);
+	} else {
+		fprintf(f, "%ld\n", bootid);
+		fclose(f);
+	}
+	return 0;
+}
+#endif
+
+
+#ifdef PLATFORM_SOLARIS
+int createbootid(void) {
+	FILE *f;
+	int fd;
+	struct utmp ut;
+	int found = 0;
+	time_t bootid = 0;
+
+	fd = open (UTMP_FILE, O_RDONLY);
+	if (fd >= 0) {
+		while(!found)
+		{
+			if (read(fd, &ut, sizeof(ut)) < 0) {
+				found = -1;
+			} else if (ut.ut_type==BOOT_TIME) {
+				found = 1;
+			}
+		}
+		close(fd);
+	}
+
+	if (found == 1) bootid = ut.ut_time;
+
+	f = fopen(FILE_BOOTID, "w");
+	if (!f) {
+		printf("Error writing bootid file, exiting!\n");  exit(-1);
+	} else {
+		fprintf(f, "%ld\n", bootid);
+		fclose(f);
+	}
+	return 0;
+}
+#endif
+
+#ifdef PLATFORM_HPUX
+int createbootid(void) {
+	FILE *f;
+	struct pst_static _pst_static;
+	
+	pstat_getstatic( &_pst_static, sizeof(_pst_static), (size_t)1, 0);
+	f=fopen(FILE_BOOTID, "w");
+	if (!f) {
+		printf("Error writing bootid file, exiting!\n");  exit(-1);
+	} else {
+		fprintf(f, "%ld\n", _pst_static.boot_time);
+		fclose(f);
+	}
+	return 0;
+}
+#endif
+
+#ifdef PLATFORM_UNKNOWN
+int createbootid(void) {
+	FILE *f;
+	time_t bootid=0;
+	
+	bootid=time(0);
+	
+	f = fopen(FILE_BOOTID, "w");
+	if (!f) {
+		printf("Error writing bootid file, exiting!\n");  exit(-1);
+	} else {
+		fprintf(f, "%ld\n", bootid);
+		fclose(f);
+	}
+	return 0;
+}
+#endif
+
+time_t readbootid(void) {
+#ifdef PLATFORM_BSD
+	time_t bootid = 0;
+	struct timeval boottime;
+	int mib[2];
+	size_t size;
+
+	mib[0] = CTL_KERN;
+	mib[1] = KERN_BOOTTIME;
+	size = sizeof (boottime);
+	if (sysctl (mib, 2, &boottime, &size, NULL, 0) != -1 && boottime.tv_sec != 0) {
+		bootid = boottime.tv_sec;
+	}
+
+	return bootid;
+#else
+	FILE *f;
+	char str[256];
+
+	f=fopen(FILE_BOOTID, "r");
+	if (!f) {
+		printf("Error reading boot id from file, exiting!\n\nYou probably forgot to create a bootid with with the -b option.\nYou really want the system to do this on bootup, read the INSTALL file!\n");
+		exit(-1);
+	}
+	fgets(str, sizeof(str), f);
+	fclose(f);
+	return atoi(str);
+#endif
+}
+
+int compare_urecs(Urec *a, Urec *b, int sort_by) {
+	if (sort_by == 1) {
+		return a->btime - b->btime;
+	} else if (sort_by == -1) {
+		return b->btime - a->btime;
+	} else if (sort_by == 2) {
+		return strcmp(a->sys, b->sys);
+	} else if (sort_by == -2) {
+		return strcmp(b->sys, a->sys);
+	}
+
+	return 0;
+}
+
+Urec* sort_urec(Urec* list, int sort_by) {
+/*
+ * sort_by:
+ *      0: Nothing
+ *      1: Boottime
+ *     -1: Reverse Boottime
+ *      2: Sysinfo
+ *     -2: Reverse Sysinfo
+ */
+
+	Urec *p, *q, *e, *tail;
+	int insize, nmerges, psize, qsize, i;
+
+	insize = 1;
+
+	for (;;) {
+		p = list;
+		list = NULL;
+		tail = NULL;
+
+		nmerges = 0;  /* count number of merges we do in this pass */
+
+		while (p) {
+			nmerges++;  /* there exists a merge to be done */
+			/* step `insize' places along from p */
+			q = p;
+			psize = 0;
+			for (i = 0; i < insize; i++) {
+				psize++;
+				q = q->next;
+				if (!q) break;
+			}
+
+			/* if q hasn't fallen off end, we have two lists to merge */
+			qsize = insize;
+
+			/* now we have two lists; merge them */
+			while (psize > 0 || (qsize > 0 && q)) {
+				/* decide whether next element of merge comes from p or q */
+				if (psize == 0) {
+					/* p is empty; e must come from q. */
+					e = q; q = q->next; qsize--;
+				} else if (qsize == 0 || !q) {
+					/* q is empty; e must come from p. */
+					e = p; p = p->next; psize--;
+				} else if (compare_urecs(p,q,sort_by) <= 0) {
+					/* First element of p is lower (or same);
+					 * e must come from p. */
+					e = p; p = p->next; psize--;
+				} else {
+					/* First element of q is lower; e must come from q. */
+					e = q; q = q->next; qsize--;
+				}
+
+				/* add the next element to the merged list */
+				if (tail) {
+					tail->next = e;
+				} else {
+					list = e;
+				}
+				tail = e;
+			}
+
+			/* now p has stepped `insize' places along, and q has too */
+			p = q;
+		}
+		tail->next = NULL;
+
+		/* If we have done only one merge, we're finished. */
+		if (nmerges <= 1) return list; /* allow for nmerges==0, the empty list case */
+
+		/* Otherwise repeat, merging lists twice the size */
+		insize *= 2;
+	}
+}
---
+++ src/uprecords.c
@@ -0,0 +1,450 @@
+/*
+uptimed - Copyright (c) 1998-2004 Rob Kaper <rob@unixcode.org>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 dated June, 1991.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program;  if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave., Cambridge, MA 02139, USA.
+*/
+
+#include "../config.h"
+#include "uprecords.h"
+
+#ifdef HAVE_GETOPT_H
+#include <getopt.h>
+#endif
+
+#define SYSWIDTH 24
+
+Urec	*u_current;
+time_t	first, prev, tenth, second;
+int		runas_cgi=0, show_max=10, show_milestone=0, layout=PRE, run_loop=0, update_interval=5;
+int		sort_by=0, no_ansi=0, no_stats=0, no_current=0, wide_out=0;
+
+int main(int argc, char *argv[])
+{
+	/* Read config file. */
+	read_config();
+
+	/* Check if we are being run as CGI program. */
+	if (strstr(argv[0], ".cgi"))
+	{
+		runas_cgi=1;
+
+		/* Print content-type header. */
+		printf("Content-type: text/html\n\n");
+		
+		/* Read CGI config file. */
+		read_config_cgi();
+	}
+
+	scan_args(argc, argv);
+
+#ifdef PLATFORM_HPUX
+	no_ansi=1;
+#endif
+
+	if (update_interval<1)
+		update_interval=1;
+
+	if (show_max<0)
+		show_max=1;
+
+	/* Print header file. */
+	if (runas_cgi)
+		cat("uprecords.header");
+
+	/* Read current uptime and entries from logfile. */
+	u_current=add_urec(read_uptime(), readbootid(), read_sysinfo());
+	read_records(u_current->btime);
+
+	if (!runas_cgi && run_loop)
+	{
+		while(1)
+		{
+			/* Update current uptime. */
+			u_current->utime=read_uptime();
+
+			/* Current uptime might move up in the list. */
+			if ((prev && prev - u_current->utime < 0) || (first && first - u_current->utime < 0))
+			{
+				moveup();
+				first=0;
+			}
+			displayrecords(1);
+			sleep(update_interval);
+		}
+	}
+	else displayrecords(0);
+
+	/* Print footer file. */
+	if (runas_cgi)
+		cat("uprecords.footer");
+
+	return 0;
+}
+
+void displayrecords(int cls)
+{
+	Urec *u, *uprev=NULL;
+	time_t now, tmp;
+	int	i=0, currentdone=0;
+	
+	now=time(0);
+
+	/* Open output for CGI. */
+	if (runas_cgi)
+	{
+		if (layout==TABLE)
+			printf("<table border=1>\n");
+		else if (layout==LIST)
+			printf("<ol>\n");
+		else
+			printf("<pre>\n");
+	}
+	/* Clear screen. */
+	else if (cls && !no_ansi)
+		printf(CLS);
+
+	if (layout==PRE)
+	{
+		printf("   %3s %20s | %-*s %*s\n", "#", "Uptime", SYSWIDTH, "System", TIMEMAX, "Boot up");
+		print_line();
+	}
+	else if (runas_cgi && layout==TABLE)
+	{
+		printf("<th>%s</th>\n", "Position");
+		printf("<th>%s</th>\n", "Uptime");
+		printf("<th>%s</th>\n", "System");
+		printf("<th>%s</th>\n", "Boot up");
+	}
+
+	for ( u=sort_urec(urec_list, sort_by) ; u ; u=u->next )
+	{
+		if (++i<=show_max || show_max==0)
+		{
+			if (u==u_current)
+			{
+				print_entry(u->utime, u->sys, u->btime, "-> ", i, 1);
+				currentdone++;
+				if (uprev) prev=uprev->utime;
+				else
+				{
+ 					prev=0; first=0; tenth=0; second=0;
+				}
+			}
+			else
+			{
+				print_entry(u->utime, u->sys, u->btime, "   ", i, 0);
+				if (i==1) first=u->utime;
+ 				if (i==2) second=u->utime;
+ 				if (i==10) tenth=u->utime;
+			}
+		}
+		else if (u==u_current)
+		{
+			if (uprev) prev=uprev->utime;
+			break;
+		}
+		uprev=u;
+	}
+
+	if (runas_cgi && layout==LIST)
+	{
+		printf("</ol>\n");
+		printf("<ul>\n");
+	}
+
+	if (!no_current && !currentdone)
+	{
+		if (layout==PRE)
+			print_line();
+
+		print_entry(u_current->utime, u_current->sys, u_current->btime, "-> ", i, 1);
+		if (uprev) prev=uprev->utime;
+	}
+
+	if (!no_stats)
+	{
+		if (layout==PRE)
+			print_line();
+
+		if (prev && prev!=first)
+		{
+			tmp=now + prev - u_current->utime;
+			print_entry(prev - u_current->utime + 1, "at", tmp, "1up in", 0, 0);
+		}
+		if (tenth && prev<=tenth && prev!=first)
+		{
+			tmp=now + tenth - u_current->utime;
+			print_entry(tenth - u_current->utime + 1, "at", tmp, "t10 in", 0, 0);
+		}
+		if (first)
+		{
+			tmp=now + first - u_current->utime;
+			print_entry(first - u_current->utime + 1, "at", tmp, "no1 in", 0, 0);
+		} else {
+			tmp=now + second - u_current->utime;
+			print_entry(u_current->utime - second - 1, "since", tmp, "NewRec", 0, 0);
+		}		
+		if (show_milestone)
+		{
+			Milestone *m;
+
+			m = find_next_milestone(u_current->utime);
+			if (m!=NULL)
+			{
+				tmp=now + m->time - u_current->utime;
+				print_entry(m->time - u_current->utime + 1, m->desc, tmp, "mst in", 0, 0);
+			}
+		}
+	}
+
+	/* End output for CGI. */
+	if (runas_cgi)
+	{
+		if (layout==TABLE)
+			printf("</table>\n");
+		else if (layout==LIST)
+			printf("</ul>\n");
+		else
+			printf("</pre>\n");
+
+		printf("<small><a href=\"http://podgorny.cz/uptimed/\">uptimed</a> by Rob Kaper (<a href=\"mailto:rob@unixcode.org\">rob@unixcode.org</a>) - currently maintained by Radek Podgorny (<a href=\"mailto:radek@podgorny.cz\">radek@podgorny.cz</a>)</small>\n");
+	}
+}
+
+void read_config(void)
+{
+	FILE *f;
+	char str[256];
+	time_t milestone_time;
+	char *milestone_str;
+	
+	f=fopen(FILE_CONFIG, "r");
+	if (!f) return;
+		
+	fgets(str, sizeof(str), f);
+	while (!feof(f))
+	{
+		if (!strncmp(str, "MILESTONE",  9))
+		{
+			char *cp = strtok(str+10, ":");
+			milestone_time=scantime(cp);
+			milestone_str=strtok(NULL, "\n");
+			add_milestone(milestone_time, milestone_str);
+		}
+		fgets(str, sizeof(str), f);
+	}
+}
+
+void read_config_cgi(void)
+{
+	FILE *f;
+	char str[256];
+
+	f=fopen("uprecords.conf", "r");	
+	if (!f)
+		return;
+	
+	fgets(str, sizeof(str), f);
+	while(!feof(f))
+	{
+		if (!strncmp(str, "LAYOUT", 6))
+		{
+			if (!strncmp(str+7, "table", 5))
+				layout=TABLE;
+			else if (!strncmp(str+7, "list", 4))
+				layout=LIST;
+			else
+				layout=PRE;
+		}
+		else if (!strncmp(str, "SHOW_MAX", 8))
+			show_max=atoi(str+9);
+		fgets(str, sizeof(str), f);
+	}
+	fclose(f);
+}
+
+void print_entry(time_t utime, char *sys, time_t btime, char *ident, int pos, int hilite)
+{
+	char *bold = BOLD, *plain = PLAIN, *current = "";
+	char *ctimec = NULL;
+
+	if (runas_cgi)
+	{
+		bold = "<b>";
+		plain = "</b>";
+		
+		if (hilite)
+			current = " (current)";
+
+		if (!strcmp(ident, "-> "))
+			ident="-&gt; ";
+		
+		if (layout!=PRE)
+		{
+			if (!strcmp(ident, "1up in"))
+				ident="One up in";
+			else if (!strcmp(ident, "no1 in"))
+				ident="Number one in";
+		}
+	}
+
+	if (!hilite || no_ansi)
+	{
+		bold = "";
+		plain = "";
+	}
+
+	switch(layout)
+	{
+		case TABLE:
+			printf("<tr>\n");
+			if (pos)
+				printf("<td>%s%d%s%s</td>\n", bold, pos, plain, current);
+			else
+				printf("<td>%s</td>\n", ident);
+
+			printf("<td>%s%s%s</td>\n", bold, time2uptime(utime), plain);
+			printf("<td>%s%s%s</td>\n", bold, sys, plain);
+			printf("<td>%s%s%s</td>\n", bold, ctime(&btime), plain);
+
+			printf("</tr>\n");
+			break;
+		case LIST:
+			if (pos>show_max)
+				printf("<li>%s%s, %s, %s%s%s (position %d)", bold, time2uptime(utime), sys, ctime(&btime), plain, current, pos);
+			else if (pos)
+				printf("<li>%s%s, %s, %s%s%s", bold, time2uptime(utime), sys, ctime(&btime), plain, current);
+			else
+				printf("<li>%s%s %s at %s%s", bold, ident, time2uptime(utime), ctime(&btime), plain);
+			break;
+		default:
+			if((ctimec = ctime(&btime)))
+				ctimec[TIMEMAX-1] = '\0';	/* erase the ending '\n' */
+			if (!wide_out && strlen(sys) > SYSWIDTH)
+				sys[SYSWIDTH] = '\0';		/* truncate for 80 cols */
+			if (pos)
+				printf("%s%3s%3d %20s %s|%s %-*s %*s%s\n", bold, ident, pos, time2uptime(utime), plain, bold, SYSWIDTH, sys, TIMEMAX, ctimec, plain);
+			else
+				printf("%s%6s %20s %s|%s %-*s %*s%s\n", bold, ident, time2uptime(utime), plain, bold, SYSWIDTH, sys, TIMEMAX, ctimec, plain);
+	}
+}
+
+void print_line(void)
+{
+	printf("----------------------------+---------------------------------------------------\n");
+}
+
+void scan_args(int argc, char *argv[])
+{
+	int i;
+
+	while((i = getopt(argc, argv, "i:m:?acbBkKfsMwv")) != EOF)
+	{
+		switch(i)
+		{
+				case '?':
+						print_help(argv);
+						break;
+				case 'a':
+						no_ansi++;
+						break;
+				case 'b':
+						sort_by = 1;
+						no_stats++;
+						break;
+				case 'B':
+						sort_by = -1;
+						no_stats++;
+						break;
+				case 'k':
+						sort_by = 2;
+						no_stats++;
+						break;
+				case 'K':
+						sort_by = -2;
+						no_stats++;
+						break;
+				case 'c':
+						no_current++;
+						break;
+				case 'f':
+						run_loop++;
+						break;
+				case 's':
+						no_stats++;
+						break;
+				case 'w':
+						wide_out++;
+						break;
+				case 'v':
+						print_version();
+						break;
+				case 'M':
+					show_milestone++;
+					break;
+				case 'i':
+					if (isdigit(*optarg))
+						update_interval=atoi(optarg);
+					else
+					{
+						printf("%s: option requires argument -- %c\n", argv[0], i);
+						print_usage(argv);
+					}
+					run_loop++;
+					break;
+				case 'm':
+					if (isdigit(*optarg))
+						show_max=atoi(optarg);
+					else
+					{
+						printf("%s: option requires argument -- %c\n", argv[0], i);
+						print_usage(argv);
+					}
+					break;
+		}
+	}
+}
+
+void print_usage(char *argv[])
+{
+	printf("usage: %s [-?acfMswv] [-i interval] [-m count]\n", argv[0]);
+	exit(1);
+}
+
+void print_help(char *argv[])
+{
+	printf("usage: %s [OPTION]...\n\n", argv[0]);
+	printf("  -?             this help\n");
+	printf("  -a             do not print ansi codes\n");
+	printf("  -b             sort by boottime\n");
+	printf("  -B             reverse sort by boottime\n");
+	printf("  -k             sort by sysinfo\n");
+	printf("  -K             reverse sort by sysinfo\n");
+	printf("  -c             do not show current entry if not in top entries\n");
+	printf("  -f             run continously in a loop\n");
+	printf("  -s             do not print extra statistics\n");
+	printf("  -w             wide output (more than 80 cols per line)\n");
+	printf("  -i INTERVAL    use INTERVAL seconds for loop instead of 5, implies -f\n");
+	printf("  -m COUNT       show a maximum of top COUNT entries instead of 10\n");
+	printf("  -M             show next milestone\n");
+	printf("  -v             version information\n");
+	exit(0);
+}
+
+void print_version(void)
+{
+	printf("uprecords " VERSION " by Rob Kaper <rob@unixcode.org>\n");
+	printf("enhanced and maintained by Radek Podgorny <radek@podgorny.cz>\n");
+	exit(0);
+}
