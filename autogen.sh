f494297ad63d75f3c6a8563ef7f5b981c5fa1f90
Radek Podgorny <radek@podgorny.cz>
2014-10-29 12:40:56 +0100

    removed some regenerable autoshit files. updated autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -1,27 +1,50 @@
-#!/bin/sh
-# Run this to generate all the initial makefiles, etc.
+#! /bin/sh
+# this is stolen from https://github.com/rakshasa/rtorrent/blob/master/autogen.sh and modified slightly
 
-test -n "$srcdir" || srcdir=`dirname "$0"`
-test -n "$srcdir" || srcdir=.
+echo aclocal...
+(aclocal --version) < /dev/null > /dev/null 2>&1 || {
+	echo aclocal not found
+	exit 1
+}
 
-olddir=`pwd`
-cd $srcdir
+aclocal -I . ${ACLOCAL_FLAGS} || exit 1
 
-AUTORECONF=`which autoreconf`
-if test -z $AUTORECONF; then
-        echo "*** No autoreconf found, please intall it ***"
-        exit 1
+echo autoheader...
+(autoheader --version) < /dev/null > /dev/null 2>&1 || {
+	echo autoheader not found
+	exit 1
+}
+
+autoheader || exit 1
+
+echo -n "libtoolize... "
+if ( (glibtoolize --version) < /dev/null > /dev/null 2>&1 ); then
+	echo "using glibtoolize"
+	glibtoolize --automake --copy --force || exit 1
+elif ( (libtoolize --version) < /dev/null > /dev/null 2>&1 ) ; then
+	echo "using libtoolize"
+	libtoolize --automake --copy --force || exit 1
+else
+    echo "libtoolize nor glibtoolize not found"
+    exit 1
 fi
 
-INTLTOOLIZE=`which intltoolize`
-if test -z $INTLTOOLIZE; then
-        echo "*** No intltoolize found, please install the intltool package ***"
-        exit 1
-fi
+echo automake...
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+	echo automake not found
+	exit 1
+}
 
-autopoint --force
-AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install --verbose
+automake --add-missing --copy --gnu || exit 1
 
-cd $olddir
-test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+echo autoconf...
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo autoconf not found
+	exit 1
+}
 
+autoconf || exit 1
+
+echo ready to configure
+
+exit 0
--- bootstrap.sh
+++
@@ -1,3 +0,0 @@
-aclocal
-automake
-autoconf
